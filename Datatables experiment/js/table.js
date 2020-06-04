f8c947c7180d66b0be4fb7dbcd3fdd06e2a03717
ReyRizki <reynaldirizki97.rr@gmail.com>
2020-06-04 21:03:56 +0700

    test date formating and change language of table head

--- Datatables experiment/js/table.js
+++ Datatables experiment/js/table.js
@@ -1,7 +1,10 @@
-$(document).ready(function () {
-    $('#example').DataTable({
+$(document).ready(function () 
+{
+    $('#example').DataTable(
+    {
         "ajax": "../JSON files/regionalSongData.json",
-        "columns": [
+        "columns":
+        [
             {
                 "data": "video",
                 "render": function (data, type, row, meta) 
@@ -16,7 +19,73 @@ $(document).ready(function () {
                     return '<a href="' + data.url + '" target="_blank">' + data.name + '<a/>';
                 }
             },
-            { "data": "regions" }
+            { 
+                "data": "regions" 
+            },
+            {
+                "data": "anotationDate",
+                "render": function(data, type, row, meta)
+                {
+                    var dateSplit = data.split('-');
+
+                    if (type === "display" || "type" == "filter")
+                    {
+                        var month = "";
+
+                        switch (dateSplit[1])
+                        {
+                        case "01":
+                            month = "Januari";
+                            break;
+                        case "02":
+                            month = "Februari";
+                            break;
+                        case "03":
+                            month = "Maret";
+                            break;
+                        case "04":
+                            month = "April";
+                            break;
+                        case "05":
+                            month = "Mei";
+                            break;
+                        case "06":
+                            month = "Juni";
+                            break;
+                        case "07":
+                            month = "Juli";
+                            break;
+                        case "08":
+                            month = "Agustus";
+                            break;
+                        case "09":
+                            month = "September";
+                            break;
+                        case "10":
+                            month = "Oktober";
+                            break;
+                        case "11":
+                            month = "November";
+                            break;
+                        case "12":
+                            month = "Desember";
+                            break;
+                        default:
+                            break;
+                        }
+
+                        return dateSplit[0] + ' ' + month + ' ' + dateSplit[2];
+                    }
+                    else if (type == "sort")
+                    {
+                        return dateSplit[2] + dateSplit[1] + dateSplit[0];
+                    }
+                    else
+                    {
+                        return data;
+                    }
+                }
+            }
         ]
     });
 });
