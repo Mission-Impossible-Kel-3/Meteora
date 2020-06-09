489989d2860ca0e597553c0ef601fa4515684860
ReyRizki <reynaldirizki97.rr@gmail.com>
2020-06-09 14:41:37 +0700

    add number of channels

--- js/createChart.js
+++ js/createChart.js
@@ -1,46 +1,75 @@
 $(document).ready(function () {
-    var statistic = []
-
+    
     $.getJSON("../JSON files/regionalSongData.json", function (data) 
     {
+        var regionStatistic = [];
+        var channelStatistic = [];
+
+        console.log(data.data.length);
         $.each(data.data, function(i, song) 
         {
+            var l = 0;
+            var found2 = false;
+            // console.log(song.channel.name);
+            while (l < channelStatistic.length && !found2)
+            {
+                if (song.channel.name == channelStatistic[l])
+                    found2 = true;
+                else
+                    l++;
+            }
+
+            if (!found2)
+                channelStatistic.push(song.channel.name);
+
             $.each(song.regions, function(j, regionName)
             {
                 var found = false;
                 var k = 0;
 
-                while (k < statistic.length && !found)
+                while (k < regionStatistic.length && !found)
                 {
-                    // if (regionName.localCompare(statistic[k].label) == 0)
-                    if (regionName == statistic[k].label)
+                    if (regionName == regionStatistic[k].label)
                     {
-                        statistic[k].y++;
+                        regionStatistic[k].y++;
                         found = true;
                     }
                     else
-                    {
                         k++;
-                    }
                 }
 
                 if (!found)
-                {
-                    statistic.push({label:regionName, y:1});
-                }
-                
-                // console.log(regionName);
+                    regionStatistic.push({label:regionName, y:1});
             });
         });
-        // document.getElementById("numberOfVideos").innerHTML = 144;
-        $("#numberOfVideosListIndex").append('<span class="counter text-success" id="numberOfVideos">' + data.data.length + '</span>');
-        // $("#numberOfVideosListIndex").append('<span class="counter text-success"' + data.data.length + '</span>');
-        $("#numberOfRegionsListIndex").append('<span class="counter text-info">' + statistic.length + '</span>');
+
+        console.log(channelStatistic.length);
+        
+        $("#numberOfVideosListIndex").append('<span class="counter text-success">' + data.data.length + '</span>');
+        $("#numberOfChannelsListIndex").append('<span class="counter text-purple">' + channelStatistic.length + '</span>');
+        $("#numberOfRegionsListIndex").append('<span class="counter text-info">' + regionStatistic.length + '</span>');
+
+        var options = {
+            animationEnabled: true,
+            data: [
+                {
+                    type: "doughnut",
+                    innerRadius: "60%",
+                    showInLegend: true,
+                    legendText: "{label}",
+                    indexLabel: "{label}: #percent%",
+                    dataPoints: regionStatistic
+                }
+            ]
+        };
+    
+        var chart = new CanvasJS.Chart("chartContainer", options);
+        chart.render();
     });
     
-    console.log(statistic);
+    // console.log(regionStatistic);
 
-    // var statistic = [
+    // var regionStatistic = [
     //     { label: "Department Stores", y: 6492917 },
     //     { label: "Discount Stores", y: 7380554 },
     //     { label: "Stores for Men / Women", y: 1610846 },
@@ -48,21 +77,21 @@ $(document).ready(function () {
     //     { label: "All other outlets", y: 900000 }
     // ]
 
-    var options = {
-        animationEnabled: true,
-        data: [
-            {
-                type: "doughnut",
-                innerRadius: "60%",
-                showInLegend: true,
-                legendText: "{label}",
-                indexLabel: "{label}: #percent%",
-                dataPoints: statistic
-            }
-        ]
-    };
+    // var options = {
+    //     animationEnabled: true,
+    //     data: [
+    //         {
+    //             type: "doughnut",
+    //             innerRadius: "60%",
+    //             showInLegend: true,
+    //             legendText: "{label}",
+    //             indexLabel: "{label}: #percent%",
+    //             dataPoints: regionStatistic
+    //         }
+    //     ]
+    // };
 
-    var chart = new CanvasJS.Chart("chartContainer", options);
-    chart.render();
+    // var chart = new CanvasJS.Chart("chartContainer", options);
+    // chart.render();
     // $("#chartContainer").CanvasJSChart(options);
 });
