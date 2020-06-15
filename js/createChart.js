c28dd5e8d3044ea74a237358e392910763eabe29
afdalrdh <afdalramdan@gmail.com>
2020-06-15 16:49:34 +0700

    tampilan done

--- js/createChart.js
+++ js/createChart.js
@@ -1,73 +1,100 @@
 $(document).ready(function () {
-    
+
     $.getJSON("https://mission-impossible-kel-3.github.io/Meteora/JSON%20files/regionalSongData.json", function (data)
-    // $.getJSON("../JSON files/regionalSongData.json", function (data) 
-    {
-        var regionStatistic = [];
-        var channelStatistic = [];
-
-        console.log(data.data.length);
-        $.each(data.data, function(i, song) 
+        // $.getJSON("../JSON files/regionalSongData.json", function (data) 
         {
-            var l = 0;
-            var found2 = false;
-            // console.log(song.channel.name);
-            while (l < channelStatistic.length && !found2)
-            {
-                if (song.channel.name == channelStatistic[l])
-                    found2 = true;
-                else
-                    l++;
-            }
+            var regionStatistic = [];
+            var channelStatistic = [];
 
-            if (!found2)
-                channelStatistic.push(song.channel.name);
-
-            $.each(song.regions, function(j, regionName)
-            {
-                var found = false;
-                var k = 0;
-
-                while (k < regionStatistic.length && !found)
-                {
-                    if (regionName == regionStatistic[k].label)
-                    {
-                        regionStatistic[k].y++;
-                        found = true;
-                    }
+            console.log(data.data.length);
+            $.each(data.data, function (i, song) {
+                var l = 0;
+                var found2 = false;
+                // console.log(song.channel.name);
+                while (l < channelStatistic.length && !found2) {
+                    if (song.channel.name == channelStatistic[l])
+                        found2 = true;
                     else
-                        k++;
+                        l++;
                 }
 
-                if (!found)
-                    regionStatistic.push({label:regionName, y:1});
+                if (!found2)
+                    channelStatistic.push(song.channel.name);
+
+                $.each(song.regions, function (j, regionName) {
+                    var found = false;
+                    var k = 0;
+
+                    while (k < regionStatistic.length && !found) {
+                        if (regionName == regionStatistic[k].label) {
+                            regionStatistic[k].y++;
+                            found = true;
+                        } else
+                            k++;
+                    }
+
+                    if (!found)
+                        regionStatistic.push({
+                            label: regionName,
+                            y: 1
+                        });
+                });
             });
-        });
 
-        console.log(channelStatistic.length);
-        
-        $("#numberOfVideosListIndex").append('<span class="counter text-success">' + data.data.length + '</span>');
-        $("#numberOfChannelsListIndex").append('<span class="counter text-purple">' + channelStatistic.length + '</span>');
-        $("#numberOfRegionsListIndex").append('<span class="counter text-info">' + regionStatistic.length + '</span>');
+            console.log(channelStatistic.length);
 
-        var options = {
-            animationEnabled: true,
-            data: [
-                {
+            $("#numberOfVideosListIndex").append('<span class="counter text-success">' + data.data.length + '</span>');
+            $("#numberOfChannelsListIndex").append('<span class="counter text-purple">' + channelStatistic.length + '</span>');
+            $("#numberOfRegionsListIndex").append('<span class="counter text-info">' + regionStatistic.length + '</span>');
+
+            CanvasJS.addColorSet("customColorSet", [
+                "#393f63",
+                "#e5d8B0",
+                "#ffb367",
+                "#f98461",
+                "#d9695f",
+                "#e05850",
+            ]);
+
+            var options = {
+                animationEnabled: true,
+                colorSet: "customColorSet",
+                data: [{
                     type: "doughnut",
-                    innerRadius: "60%",
+                    innerRadius: "70%",
                     showInLegend: true,
                     legendText: "{label}",
                     indexLabel: "{label}: #percent%",
                     dataPoints: regionStatistic
+                }],
+                animationDuration: 800,
+                animationEnabled: true,
+                backgroundColor: "transparent",
+                colorSet: "customColorSet",
+                theme: "theme2",
+                legend: {
+                    fontFamily: "calibri",
+                    fontSize: 14,
+                    horizontalAlign: "left",
+                    verticalAlign: "center",
+                    itemTextFormatter: function (e) {
+                        return e.dataPoint.name + ": " + Math.round(e.dataPoint.y / activeUsers * 100) + "%";
+                    }
+                },
+                title: {
+                    dockInsidePlotArea: true,
+                    fontSize: 55,
+                    fontWeight: "normal",
+                    horizontalAlign: "center",
+                    verticalAlign: "center",
+                    text: data.data.length
                 }
-            ]
-        };
-    
-        var chart = new CanvasJS.Chart("chartContainer", options);
-        chart.render();
-    });
-    
+            };
+
+            var chart = new CanvasJS.Chart("chartContainer", options);
+            chart.render();
+        });
+
     // console.log(regionStatistic);
 
     // var regionStatistic = [
--- js/table.js
+++ js/table.js
@@ -1,87 +1,88 @@
-$(document).ready(function () 
-{
-    $('#example').DataTable(
-    {
-        "ajax": "https://mission-impossible-kel-3.github.io/Meteora/JSON%20files/regionalSongData.json",
-        "columns":
-        [
+$(document).ready(function () {
+    $('#example').DataTable({
+        bAutoWidth: false,
+        aoColumns: [{
+                sWidth: '25%'
+            },
             {
+                sWidth: '25%'
+            },
+            {
+                sWidth: '25%'
+            },
+            {
+                sWidth: '25%'
+            }
+        ],
+        "ajax": "https://mission-impossible-kel-3.github.io/Meteora/JSON%20files/regionalSongData.json",
+        "columns": [{
                 "data": "video",
-                "render": function (data, type, row, meta) 
-                {
+                "render": function (data, type, row, meta) {
                     return '<a href="' + data.url + '" target="_blank">' + data.title + '<a/>';
                 }
             },
             {
                 "data": "channel",
-                "render": function (data, type, row, meta) 
-                {
+                "render": function (data, type, row, meta) {
                     return '<a href="' + data.url + '" target="_blank">' + data.name + '<a/>';
                 }
             },
-            { 
-                "data": "regions" 
+            {
+                "data": "regions"
             },
             {
                 "data": "annotationDate",
-                "render": function(data, type, row, meta)
-                {
+                "render": function (data, type, row, meta) {
                     var dateSplit = data.split('-');
 
-                    if (type === "display" || "type" == "filter")
-                    {
+                    if (type === "display" || "type" == "filter") {
                         var month = "";
 
-                        switch (dateSplit[1])
-                        {
-                        case "01":
-                            month = "Januari";
-                            break;
-                        case "02":
-                            month = "Februari";
-                            break;
-                        case "03":
-                            month = "Maret";
-                            break;
-                        case "04":
-                            month = "April";
-                            break;
-                        case "05":
-                            month = "Mei";
-                            break;
-                        case "06":
-                            month = "Juni";
-                            break;
-                        case "07":
-                            month = "Juli";
-                            break;
-                        case "08":
-                            month = "Agustus";
-                            break;
-                        case "09":
-                            month = "September";
-                            break;
-                        case "10":
-                            month = "Oktober";
-                            break;
-                        case "11":
-                            month = "November";
-                            break;
-                        case "12":
-                            month = "Desember";
-                            break;
-                        default:
-                            break;
+                        switch (dateSplit[1]) {
+                            case "01":
+                                month = "Januari";
+                                break;
+                            case "02":
+                                month = "Februari";
+                                break;
+                            case "03":
+                                month = "Maret";
+                                break;
+                            case "04":
+                                month = "April";
+                                break;
+                            case "05":
+                                month = "Mei";
+                                break;
+                            case "06":
+                                month = "Juni";
+                                break;
+                            case "07":
+                                month = "Juli";
+                                break;
+                            case "08":
+                                month = "Agustus";
+                                break;
+                            case "09":
+                                month = "September";
+                                break;
+                            case "10":
+                                month = "Oktober";
+                                break;
+                            case "11":
+                                month = "November";
+                                break;
+                            case "12":
+                                month = "Desember";
+                                break;
+                            default:
+                                break;
                         }
 
                         return dateSplit[0] + ' ' + month + ' ' + dateSplit[2];
-                    }
-                    else if (type == "sort")
-                    {
+                    } else if (type == "sort") {
                         return dateSplit[2] + dateSplit[1] + dateSplit[0];
-                    }
-                    else
-                    {
+                    } else {
                         return data;
                     }
                 }
